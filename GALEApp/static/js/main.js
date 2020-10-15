6b2e23e64173925cc2e36a3d5d0836befb515ab8
arvind <arvindpal.pal@gmail.com>
2020-10-15 14:12:35 +0530

    changes

--- GALEApp/static/js/main.js
+++ GALEApp/static/js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     "use strict";
     jQuery.ajaxSettings.traditional = true;
 
-    const API_URL = 'http://127.0.0.1:8001'
+    //const API_URL = 'http://127.0.0.1:8001'
+    const API_URL = 'https://gale-ipl.azurewebsites.net'
 
     $(document.body).on('change', '.season-select', function(e){
         e.preventDefault();
